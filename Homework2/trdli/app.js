e2ea14aef7db02677b151ae1f20b87bce2b5d67f
Hengyi Li <trdli@ucdavis.edu>
2024-10-12 20:26:38 -0700

    Basic structure v1.0.0.0 alpha

--- Homework2/trdli/app.js
+++ Homework2/trdli/app.js
@@ -1,5 +1,5 @@
-import { TextMessage, Title } from './src/greetings';
-import { VisualizeLayout_grid } from './src/VisualizeLayout';
+import { Title } from './src/greetings';
+import { VisualizeLayout_grid, mountChart1 } from './src/VisualizeLayout';
 import './style.css';
 
 // You can manage your layout through CSS, or this template also has materialize library supported.
@@ -8,10 +8,9 @@ import './style.css';
 document.querySelector('#app').innerHTML = `
   <div id='main-container' class='d-flex flex-column flex-nowrap'>
   ${ Title }
-  ${ TextMessage }
   ${ VisualizeLayout_grid }
   </div>
 `;
 
-mountBarChart();
-mountCounter(document.querySelector('#counter-button'));
+mountChart1();
+
--- Homework2/trdli/src/greetings.js
+++ Homework2/trdli/src/greetings.js
@@ -1,14 +1,7 @@
 export const Title = (`
-    <div>
+    <div class='Greeting_div'>
         <h1>Car Price Visualization</h1>
-    </div>
-`);
-export const TextMessage = (`
-    <div class='card' id='note-card'>
-        <div class="card-content">
-            <p>
-            Hi there! For this project, we would like to visualize the car price data.
-            </p>
-        </div>
+        <i> The ultimate guide to buy a car </i>
+        <p>By Hengyi Li</p>
     </div>
 `);

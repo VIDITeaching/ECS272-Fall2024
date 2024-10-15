40b467a7aa6b464b6cc36ad446374b0daa6692d1
TheRealMilesLee <grandtheftauto710@outlook.com>
2024-10-14 20:18:50 -0700

    Update the graph1

--- Homework2/trdli/src/VisualizeLayout.js
+++ Homework2/trdli/src/VisualizeLayout.js
@@ -32,7 +32,6 @@ export const Graph1_OverallView = (
   `<div class='chart-container d-flex' id='bar-container-graph1'>
         <svg id='Graph1' width='100%' height='100%'>
         </svg>
-        <p> Here we place the chart 1 </p>
     </div>`
 );
 export const Graph2_DetailView = (
@@ -106,7 +105,23 @@ let graph_1_value = await d3.csv('../data/car_prices.csv', (d) =>
     year: +d.year,
     make: d.make,
     model: d.model,
-    trim: d.trim
+    body: d.body,
+    odometer: +d.odometer,
+    price: +d.sellingprice
+  };
+});
+
+let graph_2_value = await d3.csv('../data/car_prices.csv', (d) =>
+{
+  return {
+    make: d.make
+  };
+});
+
+let graph_3_value = await d3.csv('../data/car_prices.csv', (d) =>
+{
+  return {
+    year: +d.year
   };
 });
 // Sort the data by year
@@ -129,76 +144,66 @@ export function mountChart3()
   chartObserver.observe(Graph3Container);
 }
 
+// Graph 1 - Parallel Coordinates. Used columns: year, make, model, Body, odometer, price.
 function Graph1_Overall()
 {
-  const margin = { top: 10, right: 100, bottom: 30, left: 50 };
-  const height = 500;
-  // select the svg tag so that we can insert(render) elements, i.e., draw the chart, within it.
-  let chartContainer = d3.select('#Graph1');
-  // Y axis is the make of the car, X axis is the year. The intersection of the two is the model of the car, and the trim of the car.
-  let yCategories = [...new Set(graph_1_value.map((d) => d.make))];
-  // This is to get the unique categories from the data using Set, then store in an array.
-  let xCategories = [...new Set(graph_1_value.map((d) => d.year))];
-  // We need a way to map our data to where it should be rendered within the svg (in screen pixels), based on the data
-  // value, so the extents and the unique values above help us define the limits.
-  // Scales are just like mapping functions y = f(x), where x refers to domain, y refers to range.
-  //      In our case, x should be the data, y should be the screen pixels.
-  // We have the margin here just to leave some space
-  // In viewport (our screen), the leftmost side always refer to 0 in the horizontal coordinates in pixels (x).
-  let xScale = d3.scaleLinear()
-    .range([height - margin.bottom, margin.top]) //bottom side to the top side on the screen
-    .domain(xCategories);
+  // set the dimensions and margins of the graph
+  const margin = { top: 30, right: 10, bottom: 10, left: 0 };
+  const width = 960 - margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
 
-  // In viewport (our screen), the topmost side always refer to 0 in the vertical coordinates in pixels (y).
-  let yScale = d3.scaleBand()
-    .rangeRound([margin.left, size.width - margin.right])
-    .domain(yCategories)
-    .padding(0.1); // spacing between the categories
+  // create svg element
+  const svg = d3.select("#Graph1");
 
-  // This following part visualizes the axes along with axis labels.
-  // Check out https://observablehq.com/@d3/margin-convention?collection=@d3/d3-axis for more details
-  const xAxis = chartContainer.append('g')
-    .attr('transform', `translate(0, ${ height - margin.bottom })`)
-    .call(d3.axisBottom(xScale));
+  const data = graph_1_value;
 
-  const yAxis = chartContainer.append('g')
-    .attr('transform', `translate(${ margin.left }, 0)`)
-    .call(d3.axisLeft(yScale));
+  const dimensions = ["year", "make", "model", "body", "odometer", "price"];
 
-  const yLabel = chartContainer.append('g')
-    .attr('transform', `translate(${ 10 }, ${ height / 2 }) rotate(-90)`)
-    .append('text')
-    .text('Make')
-    .style('font-size', '.8rem');
+  // 为每个维度创建一个y比例尺
+  const y = {};
+  dimensions.forEach(dim =>
+  {
+    y[dim] = d3.scaleLinear()
+      .domain(d3.extent(data, d => d[dim]))
+      .range([height, 0]);
+  });
 
-  const xLabel = chartContainer.append('g')
-    .attr('transform', `translate(${ size.width / 2 - margin.left }, ${ height - margin.top - 5 })`)
-    .append('text')
-    .text('Year')
-    .style('font-size', '.8rem');
+  // 为x轴创建一个比例尺
+  const x = d3.scalePoint()
+    .range([0, width])
+    .padding(1)
+    .domain(dimensions);
 
-  // "g" is grouping element that does nothing but helps avoid DOM looking like a mess
-  // We iterate through each <CategoricalBar> element in the array, create a rectangle for each and indicate the coordinates, the rectangle, and the color.
-  const barEles = chartContainer.append('g')
-    .selectAll('rect')
-    .data(graph_1_value) // TypeScript expression. This always expects an array of objects.
-    .join('rect')
-    // specify the left-top coordinate of the rectangle
-    .attr('x', (d) => xScale(d.year))
-    .attr('y', (d) => yScale(d.make))
-    // specify the size of the rectangle
-    .attr('width', (d) => Math.abs(xScale(0) - xScale(d.year)))
-    .attr('height', yScale.bandwidth()) // this substraction is reversed so the result is non-negative
-    .attr('fill', 'teal');
-  console.log("hey");
-  // For transform, check out https://www.tutorialspoint.com/d3js/d3js_svg_transformation.htm, but essentially we are adjusting the positions of the selected elements.
-  const title = chartContainer.append('g')
-    .append('text') // adding the text
-    .attr('transform', `translate(${ size.width / 2 }, ${ height - margin.top + 5 })`)
-    .attr('dy', '0.5rem') // relative distance from the indicated coordinates.
-    .style('text-anchor', 'middle')
-    .style('font-weight', 'bold')
-    .text(''); // text content
+  // 添加坐标轴
+  const axis = svg.selectAll(".axis")
+    .data(dimensions)
+    .enter().append("g")
+    .attr("class", "axis")
+    .attr("transform", d => `translate(${ x(d) },0)`)
+    .each(function (d) { d3.select(this).call(d3.axisLeft(y[d])); });
+
+  // 添加坐标轴标题
+  axis.append("text")
+    .style("text-anchor", "middle")
+    .attr("y", -9)
+    .text(d => d)
+    .style("fill", "black");
+
+  // 添加平行坐标线
+  const line = d3.line()
+    .defined(d => !isNaN(d[1]))
+    .x((_, i) => x(dimensions[i]))
+    .y(d => d[1]);
+
+  svg.selectAll(".line")
+    .data(data)
+    .enter().append("path")
+    .attr("class", "line")
+    .attr("d", d => line(dimensions.map(p => [p, y[p](d[p])])))
+    .style("fill", "none")
+    .style("stroke", "steelblue")
+    .style("opacity", 0.5);
+  console.log("Graph1_Overall");
 }
 
 function Graph2_Detail()

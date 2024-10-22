e7c486bf06122744e0989b620ec29af44c03436a
Saipranav Kotamreddy <saipranav.kotamreddy@gmail.com>
2024-10-21 18:24:48 -0700

    "Moved files to proper folder"

---
+++ Homework2/saikotamreddy/src/App.tsx
@@ -0,0 +1,59 @@
+import BarChart from './components/BarChart'
+import Parallel from './components/Parallel'
+import Dendrogram from './components/Dendrogram'
+import Notes from './components/Notes'
+import { NotesWithReducer, CountProvider } from './components/NotesWithReducer';
+import {Grid, Divider} from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
+
+// Adjust the color theme for material ui
+const theme = createTheme({
+  palette: {
+    primary:{
+      main: grey[700],
+    },
+    secondary:{
+      main: grey[700],
+    }
+  },
+})
+
+// For how Grid works, refer to https://mui.com/material-ui/react-grid/
+
+function Layout() {
+  return (
+    <Grid container id="main-container" style={{ height: '100vh', flexDirection: 'column' }}>
+  <Grid container item style={{ height: '50%'}}>
+    <Grid item xs={5}>
+      <BarChart  />   
+    </Grid>
+    <Divider orientation="vertical" sx={{ height: '100%', width: '2px', bgcolor: 'black' }} />
+    <Grid item xs={6}>
+      <Parallel/>
+    </Grid>
+  </Grid>
+  <Divider sx={{ 
+        width: '100%', 
+        height: '4px', 
+        bgcolor: 'black'
+      }} /> 
+  <Grid container item style={{ height: '45%', overflow: 'hidden' }}>
+    <Grid item xs={12} style={{ height: '100%' }}>
+      <Dendrogram />
+    </Grid>
+  </Grid>
+</Grid>
+
+  )
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout />
+    </ThemeProvider>
+  )
+}
+
+export default App

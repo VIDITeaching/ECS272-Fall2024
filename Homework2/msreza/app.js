3523c7a583a06438168106c92d246c2bfe86a67a
mreza822 <129976943+mreza822@users.noreply.github.com>
2024-10-22 23:16:11 -0700

    Update app.js
    all three visualizations

--- Homework2/msreza/app.js
+++ Homework2/msreza/app.js
@@ -1,16 +1,17 @@
-import { mountBarChart, BarChart } from './src/Example'
-import { Notes, mountCounter } from './src/notes';
+import { mountHistogram, Histogram } from './src/vis-code'
+import { mountPieChart, PieChart } from './src/vis-code';
+import { mountParallelCoordinates, ParallelCoordinates } from './src/vis-code';
 import './style.css'
-
-// You can manage your layout through CSS, or this template also has materialize library supported.
-// Materialize: https://materializecss.com/getting-started.html
 
 document.querySelector('#app').innerHTML = `
   <div id='main-container' class='d-flex flex-column flex-nowrap'>
-    ${BarChart()}
-    ${Notes("This is a message sent from app.js")}
+    ${Histogram()}
+    ${PieChart()}
+    ${ParallelCoordinates()}
+    
   </div>
 `
 
-mountBarChart();
-mountCounter(document.querySelector('#counter-button'));
+mountHistogram();
+mountPieChart();
+mountParallelCoordinates();
